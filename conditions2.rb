318077d5c7ac9346600309709fb43af674c089df
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-08 22:34:33 +0300

    continue learn conditions (if else)

---
+++ conditions2.rb
@@ -0,0 +1,15 @@
+i = 10
+if i >= 10
+  puts "number greater than or equals 10"
+elsif i <= 10  
+  puts "number less than or equals 10"
+else
+  puts "number equals 10"
+end
+puts "---------------------------------"
+ if i >= 10
+  puts "number greater than or equals 10"
+end
+if i <= 10  
+  puts "number less than or equals 10"
+end
