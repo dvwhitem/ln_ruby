a3a0b62b9e003e60d1c01076430350b14481abf7
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-17 23:14:50 +0300

    class Item setter/getter

---
+++ scrincast/item.rb
@@ -0,0 +1,11 @@
+class Item
+  def initialize(options)
+    @price = options[:price]
+    @weight = options[:weight] 
+  end
+
+  attr_reader :price, :weight
+  attr_writer :price
+end
+
+p Item.new({:price => 40}).price
