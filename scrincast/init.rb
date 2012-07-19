9e632403936cfe76512cfcf970b67e484f4e9279
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-19 23:35:32 +0300

    Update class init

--- scrincast/init.rb
+++ scrincast/init.rb
@@ -1,9 +1,24 @@
 require_relative "item"
 require_relative  "cart"
 
+=begin
 cart = Cart.new
 cart.add_item(Item.new)
 cart.add_item(Item.new)
+
+cart.validate
 p cart.items
 cart.remove_item
+p cart.items
+=end
+
+item1 = Item.new({:price => 10, :weight => 120, :name => "Car"})
+item2 = Item.new({:weight => 120, :name => "Car"})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+
+p cart.items
+cart.delete_invalid_items
 p cart.items
