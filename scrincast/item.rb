e32b8e6538fad8e664954aa6faf836baf2e2ab70
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-26 22:06:41 +0300

    update item class (parent)

--- scrincast/item.rb
+++ scrincast/item.rb
@@ -1,16 +1,14 @@
 class Item
   def initialize(options={})
     @price = options[:price]
-    @weight = options[:weight]
     @name = options[:name]
   end
 
-  attr_reader :price, :weight, :name
+  attr_reader :price, :name
   attr_writer :price
 
   def info
     yield(price)
-    yield(weight)
     yield(name)
   end
 end
