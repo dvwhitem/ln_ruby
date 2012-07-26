8b0add89ed51de5e61a44c9c7c42d80445ad9ef6
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-26 22:07:31 +0300

    added real_item class

---
+++ scrincast/real_item.rb
@@ -0,0 +1,14 @@
+class RealItem < Item
+  
+  attr_reader :weight
+  
+  def initialize(options={})
+    @weight = options[:weight]
+    super
+  end 
+
+  def info()
+    yield(weight)
+    super
+  end 
+end
