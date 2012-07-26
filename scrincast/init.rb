8d69d55cc7f271a3d22e7b179025f1db3d81ad26
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2012-07-26 22:05:48 +0300

    update init (extends, polymorphism)

--- scrincast/init.rb
+++ scrincast/init.rb
@@ -1,5 +1,8 @@
 require_relative "item"
 require_relative  "cart"
+require_relative  "virtual_item.rb"
+require_relative  "real_item.rb"
+
 
 =begin
 cart = Cart.new
@@ -12,8 +15,8 @@ cart.remove_item
 p cart.items
 =end
 
-item1 = Item.new({:price => 10, :weight => 120, :name => "Car"})
-item2 = Item.new({:weight => 120, :name => "Car"})
+item1 = VirtualItem.new({:price => 10, :weight => 120, :name => "Car"})
+item2 = RealItem.new({:weight => 120, :name => "Car"})
 
 cart = Cart.new
 cart.add_item item1
@@ -22,3 +25,8 @@ cart.add_item item2
 p cart.items
 cart.delete_invalid_items
 p cart.items
+
+p item1.respond_to?(:weight)
+p item2.respond_to?(:weight)
+
+
